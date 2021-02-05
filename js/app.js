5a145607e24ec82fd655c5df248347403c48ba46
Kécia Bastos <kecia.bastos@gmail.com>
2021-02-05 01:38:36 -0300

    VERSAO 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,77 @@
 function formatamoeda(numero) {
   return numero.toFixed(2).replace(".", ",")
 }
+
+function mudacor(input1, input2) {
+  if (input1 == 0) {
+    document.getElementById('selectEnergia').style.borderColor = "#FF0F0F";
+  }
+  if (input2 == 0) {
+    document.getElementById('selectProteina1').style.borderColor = "#FF0F0F";
+  }
+}
+
+function mudacormateria(tipo) {
+  if (tipo == 1) {
+    document.getElementById('amn').className = "navegacaomenu";
+    document.getElementById('ams').className = "";
+  }
+  if (tipo == 2) {
+    document.getElementById('ams').className = "navegacaomenu";
+    document.getElementById('amn').className = "";
+  }
+}
+
+function mudacorpb() {
+  document.getElementById('inputPb').style.borderColor = "#FF0F0F";
+}
+
+function resetacor() {
+  document.getElementById('selectEnergia').style.borderColor = "#E5E5E5";
+  document.getElementById('selectProteina1').style.borderColor = "#E5E5E5";
+}
+
+//MOEDA
+function moeda(a, e, r, t) {
+  let n = ""
+    , h = j = 0
+    , u = tamanho2 = 0
+    , l = ajd2 = ""
+    , o = window.Event ? t.which : t.keyCode;
+  if (13 == o || 8 == o)
+    return !0;
+  if (n = String.fromCharCode(o),
+    -1 == "0123456789".indexOf(n))
+    return !1;
+  for (u = a.value.length,
+    h = 0; h < u && ("0" == a.value.charAt(h) || a.value.charAt(h) == r); h++)
+    ;
+  for (l = ""; h < u; h++)
+    -1 != "0123456789".indexOf(a.value.charAt(h)) && (l += a.value.charAt(h));
+  if (l += n,
+    0 == (u = l.length) && (a.value = ""),
+    1 == u && (a.value = "0" + r + "0" + l),
+    2 == u && (a.value = "0" + r + l),
+    u > 2) {
+    for (ajd2 = "",
+      j = 0,
+      h = u - 3; h >= 0; h--)
+      3 == j && (ajd2 += e,
+        j = 0),
+        ajd2 += l.charAt(h),
+        j++;
+    for (a.value = "",
+      tamanho2 = ajd2.length,
+      h = tamanho2 - 1; h >= 0; h--)
+      a.value += ajd2.charAt(h);
+    a.value += r + l.substr(u - 2, u)
+  }
+  return !1
+}
+
+
+
+
 function formulario() {
   var quemchamou = localStorage.getItem('tipoanimal');
   //alert(alimentos);
@@ -24,61 +95,69 @@ function formulario() {
   var preco2 = inputPreco2.value;
   var preco3 = inputPreco3.value;
   var preco4 = inputPreco4.value;
-  var porcentopb = inputPb.value;
+  var porcentopb = (inputPb.value / 100);
   var numrebanho = inputRebanho.value;
 
-  console.log(exigencias[quemchamou]);
+  if ((energia1 == 0) || (proteina1 == 0)) {
+    mudacor(energia1, proteina1);
+  } else {
+    resetacor();
+    const alimentoCalculo = [];
 
-  const alimentoCalculo = [];
+    if (energia1 != 0) {
+      if (preco1) {
+        alert("ENTROU");
+        alimentos[energia1].custo = parseFloat((preco1)/alimentos[energia1].ms); //COMO SETAR O NOVO VALOR NO OBJETO
+      }
+      alimentoCalculo.push(alimentos[energia1]);
+    }
+    if (energia2 != 0) {
+      if (preco2) {
 
-  if (energia1 != 0) {
-    if (preco1) {
-      alimentos[energia1].custo = parseFloat(preco1); //COMO SETAR O NOVO VALOR NO OBJETO
+        alimentos[energia2].custo = parseFloat((preco2)/alimentos[energia2].ms);
+      }
+      alimentoCalculo.push(alimentos[energia2]);
+    }
+    if (proteina1) {
+      if (parseFloat(preco3) > 0) {
+        alimentos[proteina1].custo = parseFloat((preco3)/alimentos[proteina1].ms);
+      }
+      alimentoCalculo.push(alimentos[proteina1]);
+    }
+    if (proteina2 != 0) {
+      if (preco4) {
+        alimentos[proteina2].custo = parseFloat((preco4)/alimentos[proteina2].ms);
+      }
+      alimentoCalculo.push(alimentos[proteina2]);
     }
 
-    alimentoCalculo.push(alimentos[energia1]);
-  }
-  if (energia2 != 0) {
-    if (preco2) {
-      alimentos[energia2].custo = parseFloat(preco2);
+    // if (!porcentopb) {
+    //   porcentopb = 0.13;
+    // } 
+    if (!numrebanho) {
+      numrebanho = 1;
     }
-    alimentoCalculo.push(alimentos[energia2]);
-  }
-  if (proteina1 != 0) {
-    if (preco3) {
-      alimentos[proteina1].custo = parseFloat(preco3);
+
+    alimentoCalculo.push(alimentos["sal"]);
+
+    const retorno = {
+      alimentos: alimentoCalculo,
+      porcentopb,
+      numrebanho,
+      animal: exigencias[quemchamou]
     }
-    alimentoCalculo.push(alimentos[proteina1]);
-  }
-  if (proteina2 != 0) {
-    if (preco4) {
-      alimentos[proteina2].custo = parseFloat(preco4);
-    }
-    alimentoCalculo.push(alimentos[proteina2]);
-  }
 
-  // if (!porcentopb) {
-  //   porcentopb = 0.13;
-  // } 
-  if (!numrebanho) {
-    numrebanho = 1;
+    montaString(retorno);
+    var ingrediente = JSON.stringify(retorno.alimentos);
+    ingredienteretorno(ingrediente);
+    nrebanho(retorno.numrebanho);
+    return retorno;
   }
-
-  const retorno = {
-    alimentos: alimentoCalculo,
-    porcentopb,
-    numrebanho,
-    animal: exigencias[quemchamou]
-  }
-  //console.log(retorno);
-  montaString(retorno);
-
-  return retorno;
 }
 
 
 function montaString(retorno) {
-  let stringParaCalculo = `Minimize \n`;
+  var stringParaCalculo = `Minimize \n`;
   stringParaCalculo += "obj: ";
   let interacao = 0;
   for (const alim of retorno.alimentos) {
@@ -101,29 +180,55 @@ function montaString(retorno) {
   stringParaCalculo += `= ${retorno.animal.ims}  `;
   stringParaCalculo += '\n';
 
-  for (const nutriente of retorno.animal.nutrientes) {
+  for (const exigencia of retorno.animal.exigenciaanimal) { //verificar a quantidade de restrições
     interacao = 0;
-    stringParaCalculo += `Restricao${restricao}: `;
-    for (const alimento of retorno.alimentos) {
-      stringParaCalculo += `+${alimento[nutriente]} x${interacao}  `;
-      interacao++;
-    }
-    restricao++;
-
-    if (retorno.porcentagempb && nutriente === "pb") {
-      stringParaCalculo += `= ${retorno.porcentopb}  `;
+    if (retorno.animal.tiporestricao[exigencia] === "=") {
+      let tolerancia = retorno.animal.tolerancia || 0.05;
+      for (const [sinal, tol] of [['>=', -tolerancia], ['<=', +tolerancia]]) {
+        stringParaCalculo += `Restricao${restricao}: `;
+        interacao = 0;
+        for (const alimento of retorno.alimentos) { //vericar as exigencias de cada animal
+          stringParaCalculo += `+${alimento[exigencia]} x${interacao}  `;
+          interacao++;
+        }
+        restricao++;
+        if (retorno.porcentopb && exigencia === "pb") { //verificar se preencheu o % de PB
+          stringParaCalculo += `${sinal} ${retorno.porcentopb * (1 + tol)}  `;
+        } else {
+          stringParaCalculo += `${sinal} ${retorno.animal[exigencia] * (1 + tol)}  `;
+        }
+        stringParaCalculo += '\n';
+        // stringParaCalculo += `Restricao${restricao}: `;
+      }
     } else {
-      stringParaCalculo += `= ${retorno.animal[nutriente]}  `;
+      stringParaCalculo += `Restricao${restricao}: `;
+      for (const alimento of retorno.alimentos) { //vericar as exigencias de cada animal
+        stringParaCalculo += `+${alimento[exigencia]} x${interacao}  `;
+        interacao++;
+      }
+      restricao++;
+
+      if (retorno.porcentopb && exigencia === "pb") { //verificar se preencheu o % de PB
+        stringParaCalculo += `${retorno.animal.tiporestricao[exigencia]} ${retorno.porcentopb}  `;
+      } else {
+        stringParaCalculo += `${retorno.animal.tiporestricao[exigencia]} ${retorno.animal[exigencia]}  `;
+      }
+      stringParaCalculo += '\n';
     }
-    stringParaCalculo += '\n';
   }
+
+
   stringParaCalculo += '\n';
   stringParaCalculo += '\n';
 
   stringParaCalculo += `Bounds \n`;
   interacao = 0;
-  for (const alim of retorno.alimentos) {
-    stringParaCalculo += `x${interacao}>=0\n`;
+  for (const alim of retorno.alimentos) { // RESTRIÇOES 
+    if ((retorno.alimentos.length - 1) == interacao) { //SAL É O ULTIMO ELEMENTO DO VETOR
+      stringParaCalculo += `x${interacao}=${retorno.animal.mineral}\n`;
+    } else {
+      stringParaCalculo += `x${interacao}>=0.1\n`;
+    }
     interacao++;
   }
 
@@ -132,19 +237,31 @@ function montaString(retorno) {
 
   stringParaCalculo += `Generals \n`;
   interacao = 0;
-  for (const alim of retorno.alimentos) {
+  for (const alim of retorno.alimentos) { // VARIÁVEIS
     stringParaCalculo += `x${interacao}\n`;
     interacao++;
   }
   stringParaCalculo += '\n';
   stringParaCalculo += '\n';
-
   stringParaCalculo += `End \n`;
 
+  //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPAGAR
   console.log(stringParaCalculo);
-  console.log(solver(stringParaCalculo));
-}
 
+  resultado = solver(stringParaCalculo); //CHAMA O SOLVER
+
+  console.log(resultado); // RESPOSTA DO SOLVER
+
+  if (resultado.z == "Não existe solução viável primal. ") {
+    alert("AJUSTA A PROTEÍNA AÍ VAI");
+    mudacorpb();
+  } else {
+    var resultadoaux = JSON.stringify(resultado);
+    atravessaretorno(resultadoaux);
+   window.location.href = "./resultadoracao.html";
+  }
+
+}
 
 
 function solver(source) {
@@ -225,67 +342,66 @@ function solver(source) {
       x: []
     };
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
-
-
-
-
-
-
-
-
-//FUNÇÃO PARA PREENCHER TABELA CRIADA NO HTML COM A RESPOSTA DO SOLVER
-var elementos;
-
-function racao() {
-  elementos = formulario();
-  alert(elementos[0].nome);
-} //TESTAR NA TELA MIKAMIX BOTÃO VOLTAR
-
-
-//PARA CRIAR TABELA NO HTML 
-var quantidademn = 0;
-
 var alimento = null;
 var quantidademn = null;
+var rastreio = 0;
+var chamada = 7;
+var key = false;
+var animais = 1;
 
-function tabelaracao() {
-  //PARA TESTAR BOTÃO SOBRE DA TELA MIKAMIXRAÇÃO
-  var a = { nome: "capim", custo: 0.02, ms: 15, pb: 10, fdn: 55, ndt: 60, cnf: 25, cinza: 4, oleo: 2, ca: 0.30, fosforo: 0.20 };
-  var b = { nome: "capimelefante", custo: 0.03, ms: 19.96, pb: 8.53, fdn: 79.20, ndt: 50.35, cnf: 12.20, cinza: 9.99, oleo: 1.95, ca: 0.38, fosforo: 0.32 };
-  var c = null;
-  var d = { nome: "capimelefante", custo: 0.03, ms: 19.96, pb: 8.53, fdn: 79.20, ndt: 50.35, cnf: 12.20, cinza: 9.99, oleo: 1.95, ca: 0.38, fosforo: 0.32 };
-  var elementos = [a, b, c, d];
-  var corpo_tabela = document.querySelector("tbody");
-  //elementos = formulario(); 
-  i = -1;
-  do {
-    i++;
-    if (elementos[i] != null) {
-      this.alimento = elementos[i].nome;
-      this.quantidademn = elementos[i].ms;
-      console.log(criar_linha_tabela(corpo_tabela));
+function totalrebanho() {
+  var rebanho = window.localStorage.getItem('totalanimal')
+  var totalanimal = JSON.parse(rebanho);
+  console.log(totalanimal);
+  return totalanimal;
+}
+
+
+function tabelaracao(materia) {
+  if (chamada == materia) {
+    rastreio = 1;
+  } else {
+    //alert("ENTREI");
+    // OBJETO COM DUAS CHAVES: Z QUE É O MENOR PREÇO ENCONTRADO COM AS COMBINAÇÕES DE X1 X2 X3 X4|| //
+    //E A SEGUNDA CHAVE DO OBJETO: X QUE É UM VETOR DO TAMANHO DO NUMERO DE INREDIENTES SELECIONADOS, 
+    //SENDO A POSICAO 0 A 3 A QUANTIDADE EM KG DOS INGREDIENTES//
+    if (rastreio === 0) {
+      //alert(rastreio);
+
+      deleta_linha_tabela(key);
+      var resultadosolver = window.localStorage.getItem('obj')
+      var retornoqtd = JSON.parse(resultadosolver);
+
+      var ingredientes = window.localStorage.getItem('objingrediente')
+      var retornoingrediente = JSON.parse(ingredientes);
+
+      var rebanho = window.localStorage.getItem('totalanimal')
+      var totalanimal = JSON.parse(rebanho);
+
+      var corpo_tabela = document.querySelector("tbody");
+
+      let int = 0;
+      for (const itenstabela of retornoingrediente) {
+        //console.log(itenstabela.ms);
+        this.alimento = itenstabela.nome;
+        if (materia === 1) {
+          this.quantidademn = parseFloat(((retornoqtd.x[int] / itenstabela.ms) * totalanimal)).toFixed(3);
+          mudacormateria(1);
+        } else {
+          this.quantidademn = parseFloat((retornoqtd.x[int] * totalanimal)).toFixed(3);
+          mudacormateria(2);
+        }
+        criar_linha_tabela(corpo_tabela);
+        int++;
+      }
     }
-  } while (i < 3);
+    chamada = materia;
+  }
+  rastreio = 0;
+  key = true;
 }
 
 function criar_linha_tabela(corpo_tabela) {
@@ -303,9 +419,31 @@ function criar_linha_tabela(corpo_tabela) {
   corpo_tabela.appendChild(linha);
 }
 
+function deleta_linha_tabela(key) {
+  if (key) {
+    var linhas = document.getElementById("tableracao").rows;
+    for (i = linhas.length - 1; i >= 1;) {
+      document.getElementById("tableracao").deleteRow(i);
+      i--;
+    }
+  }
+}
+
 function tipoanimal(codigo) {
   // alert(codigo);
   // return codigo;
   localStorage.setItem('tipoanimal', codigo);
 }
 
+function atravessaretorno(solucaosolver) {
+  localStorage.setItem('obj', solucaosolver);
+}
+
+
+function ingredienteretorno(ingrediente) {
+  localStorage.setItem('objingrediente', ingrediente);
+}
+
+function nrebanho(nanimal) {
+  localStorage.setItem('totalanimal', nanimal);
+}
